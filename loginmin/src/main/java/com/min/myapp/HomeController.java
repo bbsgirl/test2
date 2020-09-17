43dc1e49b9dabf2069a610bf7c866d891ce86073
bbsgirl <63543308+bbsgirl@users.noreply.github.com>
2020-09-17 20:21:32 +0900

    Update HomeController.java

--- loginmin/src/main/java/com/min/myapp/HomeController.java
+++ loginmin/src/main/java/com/min/myapp/HomeController.java
@@ -11,10 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
  * Handles requests for the application home page.
  */
+@SessionAttributes("id")
 @Controller
 public class HomeController {
 	@Autowired
@@ -26,35 +28,79 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model) {//모델은 리퀘스트라 생각하자
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "home";
 	}
 	
+	
 	@RequestMapping("/main")
-	public String main() {
+	public String main(Model model) {
+		
+		//메인 뿌린다 
+		boardminDto[] list = dao.getAll();
+		System.out.println(list.length+"*****");
+		model.addAttribute("list",list);
 		
 		return "main";
 	}
-
+	//회원가입========================================
 	@RequestMapping("/goInsert")
 	public String insert() {
-		
 		return "insert";
 	}
-
 	@RequestMapping("/insert")
 	public String insert(beanDto beanDto) {
-		
 		dao.insert(beanDto);
+		return "redirect:main";
+	}
+	//게시물등록====================================
+	@RequestMapping("/gob_insert")
+	public String b_insert() {
+		return "b_insert";
+	}
+	@RequestMapping("/b_insert")
+	public String b_insert(beanDto beanDto ,boardminDto boardminDto) {
+		dao.b_insert(beanDto, boardminDto);
+		return "redirect:main";
+	}
+	//로그인=====================================
+	@RequestMapping("/gologin")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping("/login")
+	public String login(beanDto beanDto , Model model) {
+		
+//		model.addAttribute("login",dao.get(new beanDto(String id)));
+		model.addAttribute("id", beanDto.getId());
+		//위에 세션이랑 연결되어있다
+		
+		return "redirect:main";
+	}
+	//삭제======================================
+	@RequestMapping("/delete")
+	public String delete(int no) {
+		
+		dao.delete(no);
+		
+		return "redirect:main";
+	}
+	//수정=======================================
+	@RequestMapping("/goupdate")
+	public String update(boardminDto boardminDto, Model model) {
+		model.addAttribute("bean",boardminDto);
+		return "update";
+	}
+	@RequestMapping("/update")
+	public String update(String content , int no) {
+		dao.update(content, no);
 		return "redirect:main";
 	}
 	
