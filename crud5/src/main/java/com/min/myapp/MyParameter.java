ddf3e723414e72ffd5afe30949f173a11fc688f9
bbsgirl <63543308+bbsgirl@users.noreply.github.com>
2020-10-31 19:20:01 +0900

    Create MyParameter.java

---
+++ crud5/src/main/java/com/min/myapp/MyParameter.java
@@ -0,0 +1,13 @@
+package com.min.myapp;
+
+public class MyParameter {
+	
+	int idx;
+	String content;
+	
+	public MyParameter(int idx, String content) {
+		this.idx=idx;
+		this.content=content;
+	}
+
+}
