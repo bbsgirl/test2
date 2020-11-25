1a2842d2fa98a2d2651b95d7cc0a72b9994a6bbc
bbsgirl <63543308+bbsgirl@users.noreply.github.com>
2020-11-25 13:57:32 +0900

    Create PnumberVo.java

---
+++ pinkmovie/src/com/spring/board/vo/PnumberVo.java
@@ -0,0 +1,29 @@
+package com.spring.board.vo;
+
+public class PnumberVo {
+	
+	private String PhNumber;
+	private String idx;
+	
+	
+
+	public String getIdx() {
+		return idx;
+	}
+
+	public void setIdx(String idx) {
+		this.idx = idx;
+	}
+
+	public String getPhNumber() {
+		return PhNumber;
+	}
+
+	public void setPhNumber(String phNumber) {
+		PhNumber = phNumber;
+	}
+
+	
+
+	
+}
