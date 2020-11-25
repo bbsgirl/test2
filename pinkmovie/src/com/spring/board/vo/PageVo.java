b4bb2944118b8814eaa752ef56d4b80f3632da13
bbsgirl <63543308+bbsgirl@users.noreply.github.com>
2020-11-25 13:57:28 +0900

    Create PageVo.java

---
+++ pinkmovie/src/com/spring/board/vo/PageVo.java
@@ -0,0 +1,15 @@
+package com.spring.board.vo;
+
+public class PageVo {
+	
+	private int pageNo;
+
+	public int getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+	
+}
